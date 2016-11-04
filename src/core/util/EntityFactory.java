baa10f7b15d538a011e5f5226b9411c320891745
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-11-04 10:34:07 +0900

    create factory base

---
+++ src/core/util/EntityFactory.java
@@ -0,0 +1,10 @@
+package core.util;
+
+import core.game.Entity;
+
+/**
+ * Created by ctare on 2016/11/04.
+ */
+public abstract class EntityFactory {
+    public abstract Entity[] create(int num);
+}
